cc2a4a3b42a5e27e913209574fbd1b179b227155
Ethan Griffin <ethangriffin002@gmail.com>
2021-12-09 13:02:47 -0800

    Day 1 part 1 solution

---
+++ solutions/Day1.js
@@ -0,0 +1,31 @@
+
+const fs = require("fs");
+const text = fs.readFileSync("../puzzleInputs/day1.txt").toString('utf-8');
+const textByLine = text.split("\n");
+
+function cleanUpInput(input) {
+    let cleanArr = []
+    for (let i = 0; i < input.length; i++) {
+        cleanArr[i] = input[i].replace(/\r/, "");
+    }
+    return cleanArr;
+}
+
+const cleanInputArr = (inputArr) => { return inputArr.map(item => parseInt(item)) };
+
+const input = [199,200,208,210,200,207,240,269,260,263];
+const input2 = cleanInputArr(cleanUpInput(textByLine));
+
+// Part 1
+
+function checkForIncreases(inputArr) {
+    let count = 0;
+    for (let i = 1; i < inputArr.length; i++) {
+        if (inputArr[i] > inputArr[i - 1]) {
+            count++;
+        }
+    }
+    return count;
+}
+
+console.log(checkForIncreases(input2));
